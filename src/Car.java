7bd2e9d878c3e01785d4301bf7de481cb96f4964
Kaisa Hakola <kaisahakola00@gmail.com>
2025-03-07 15:25:18 +0200

    fix car

---
+++ src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    String car = "hello im car";
+    String engine = "köh köh";
+
+    public void fixCar() {
+        this.engine = "vrumm vrumm";
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Car myCar = new Car();
+        System.out.println(myCar.car);
+        System.out.println(myCar.engine);
+        System.out.println("oh no car no wörk");
+        myCar.fixCar();
+        System.out.println(myCar.engine);
+        System.out.println("yes now car wörk");
     }
 }
